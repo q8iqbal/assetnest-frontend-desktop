b5e45494e549d05efb8dec36f489ba1fe23d5dd6
GoldyNajma <najma.goldy354@gmail.com>
2020-11-29 18:30:57 +0700

    Tambah EditProfilePage owner
    Masih tampilan aja

---
+++ assetnest-wpf/EditProfile/EditProfilePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Velacro.UIElements.Basic;
+
+namespace assetnest_wpf.EditProfile
+{
+    /// <summary>
+    /// Interaction logic for EditProfile.xaml
+    /// </summary>
+    public partial class EditProfilePage : MyPage
+    {
+        public EditProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
