cd727b3bcfbf1c8dceba9cbbc664a907c97b6780
Nawa <dragneel77@gmail.com>
2020-12-04 15:16:29 +0700

    nope

---
+++ assetnest-wpf/ListUser/ListUserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Velacro.Api;
+using Velacro.Basic;
+
+namespace assetnest_wpf.ListUser
+{
+    class ListUserController : MyController
+    {
+        public ListUserController(IMyView _myView) : base(_myView)
+        {
+        }
+    }
+}
