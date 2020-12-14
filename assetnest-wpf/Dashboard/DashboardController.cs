e49a18e18c95370c7a33f96ebb7b60a060079d1f
Chandra Kirana Jatu Indraswari <chandraindras31@gmail.com>
2020-12-14 13:55:13 +0700

    add dashboard

---
+++ assetnest-wpf/Dashboard/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assetnest_wpf.Dashboard
+{
+    class DashboardController
+    {
+    }
+}
